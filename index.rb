d0cf710aed27c28d3ee2f7adb031f783a15131f9
Rune Skjoldborg Madsen <rune@runemadsen.com>
2010-11-12 13:46:54 -0500

    Added pagination

--- index.rb
+++ index.rb
@@ -6,9 +6,21 @@ require 'cgi'
 
 set :views, File.dirname(__FILE__) + '/views'
 
-get "/blog" do
+get '/blog' do
   
   @posts = BlogPost.all :limit => 5, :order => [ :created_at.desc ]
-  
   erb :blogposts
+  
 end
+
+get '/blog/:page' do
+  
+  offset = (params[:page].to_i - 1) * 5
+  
+  @posts = BlogPost.all :limit => 5, :order => [ :created_at.desc ], :offset => offset
+  erb :blogposts
+  
+end
+
+
+
